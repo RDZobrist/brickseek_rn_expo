8b6cba7407d13a5fdd1f0c70e54f3caa27011cb5
Robert Zobrist <rdzobrist.dev@gmail.com>
2022-02-03 16:44:53 -0700

    fetching products and displaying low_price for each product---data is in my hands, now lets go create custom card component

--- screens/DealsScreen.tsx
+++ screens/DealsScreen.tsx
@@ -2,12 +2,41 @@ import { StyleSheet, Dimensions, View, Text } from 'react-native';
 import { useEffect, useState } from 'react';
 import { RootTabScreenProps } from '../types';
 
+
+
+
+
 const DealsScreen = ({ navigation }: RootTabScreenProps<'Deals'>) => {
   const [loading, setLoading] = useState(false);
+  const [products, fetchProducts] = useState();
+  useEffect(() => {
+    const getDealsFromApi = async () => {
+      let response = await 
+  
+      fetch('http://52.89.75.192/deals?records=60', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+     
+      });
+     const dataJSON = await response.json()
+     let products = dataJSON.products;
+      return fetchProducts(products);
+      
+    }
+    getDealsFromApi();
+    
+  });
+
   return (
     <View  style={styles.container}>
       <Text style={styles.title}>Deals</Text>
       <View style={styles.separator} />
+      <View>
+        {products ? products.map(product => {return(<Text key={product.id}>{product.low_price}</Text>)}):null}
+      </View>
     </View>
   );
 }
