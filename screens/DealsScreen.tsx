e4023fadca2c4e61656daae6f530b7f4de3052c7
Robert Zobrist <rdzobrist.dev@gmail.com>
2022-02-06 17:56:35 -0700

    added pull to refresh to deals flatlist

--- screens/DealsScreen.tsx
+++ screens/DealsScreen.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Dimensions, View, Text, FlatList, SafeAreaView, ActivityIndicator } from 'react-native';
-import { useEffect, useState } from 'react';
+import { StyleSheet, Dimensions, View, Text, FlatList, SafeAreaView, ActivityIndicator, RefreshControl } from 'react-native';
+import { useEffect, useState, useCallback } from 'react';
 import { RootTabScreenProps } from '../types';
 import ProductCard from '../components/ProductCard';
 
 const DealsScreen = ({ navigation }: RootTabScreenProps<'Deals'>) => {
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
   const [products, fetchProducts] = useState([]);
 
   const renderProduct = (product: Object) => {
@@ -13,7 +14,8 @@ const DealsScreen = ({ navigation }: RootTabScreenProps<'Deals'>) => {
   };
   const getDealsFromApi = async () => {
     setLoading(true);
-    let response = await fetch('http://52.89.75.192/deals?records=100',
+    try{
+    let response = await fetch('http://52.89.75.192/deals?records=75',
       {
         method: 'GET',
         headers: {
@@ -21,11 +23,26 @@ const DealsScreen = ({ navigation }: RootTabScreenProps<'Deals'>) => {
           'Content-Type': 'application/json'
         }
       });
-      setLoading(false);
 
     const dataJSON = await response.json();
+    dataJSON.products?setLoading(false):null
     return fetchProducts(dataJSON.products);
+    }
+    catch {
+      setLoading(false);
+      return alert('There was an error fetching your deals')
+    }
   };
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+      try {
+        getDealsFromApi();
+        setRefreshing(false);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+   
 
   useEffect(() => {
     getDealsFromApi();
@@ -38,6 +55,7 @@ const DealsScreen = ({ navigation }: RootTabScreenProps<'Deals'>) => {
         data={products}
         renderItem={renderProduct}
         keyExtractor={(product, index) => index}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       />
 
     </SafeAreaView>
@@ -49,8 +67,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width
+    height: Dimensions.get('screen').height,
+    width: Dimensions.get('screen').width
   },
   title: {
     fontSize: 20,
